cdeb38a79fa20e376e26f6d991f7a19c5608df02
AxiomDays <tobeoke@gmail.com>
2024-03-25 03:08:01 +0100

    task6

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Write a script that starts a Flask web application:"""
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+@app.route("/")
+def hello_hbnb():
+    return "Hello HBNB!"
+
+@app.route("/hbnb")
+def hbnb():
+    return "HBNB"
+
+@app.route("/c/<name>")
+def c(name):
+    return ("C {}".format(name.replace("_", " ")))
+
+@app.route("/python")
+@app.route("/python/<text>")
+def python(text='is cool'):
+    return ("Python {}".format(text.replace("_", " ")))
+
+@app.route("/number/<int:n>")
+def number(n):
+     return ("{} is a number".format(n))
+
+@app.route("/number_template/<int:n>")
+def html(n):
+    message = n
+    return ("<!DOCTYPE html><HTML lang=\"en\"><HEAD><TITLE>HBNB</TITLE></HEAD><BODY><H1>Number: {}</H1></BODY></HTML>".format(n))
+
+@app.route("/number_odd_or_even/<int:n>")
+def odd_or_even(n):
+    if (n % 2 == 0):
+        message = ("Number {} is even").format(n)
+    else:
+        message = ("Number {} is odd").format(n)
+    return render_template('6-number_odd_or_even.html', message=message)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
